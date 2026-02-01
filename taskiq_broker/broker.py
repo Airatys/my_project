9188110adc558106218d4463873f00a0ed35c8eb
Airat Sharifullin <sharifullin.airat@yandex.ru>
2026-02-01 18:58:13 +0300

    broker = PullBasedJetStreamBroker

--- taskiq_broker/broker.py
+++ taskiq_broker/broker.py
@@ -1,3 +1,4 @@
-from taskiq_nats import NatsBroker
+from taskiq_nats import NatsBroker, PullBasedJetStreamBroker
 
-broker = NatsBroker(servers="nats://127.0.0.1:4222", queue="taskiq_queue")
+# broker = NatsBroker(servers="nats://127.0.0.1:4222", queue="taskiq_queue")
+broker = PullBasedJetStreamBroker(servers="nats://127.0.0.1:4222", queue="taskiq_queue")
